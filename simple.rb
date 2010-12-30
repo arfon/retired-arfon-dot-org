48365f559c38f3e7d52a98e42247f35087e755dd
Arfon Smith <arfon.smith@gmail.com>
2010-12-30 22:05:02 +0000

    Count

--- simple.rb
+++ simple.rb
@@ -1,6 +1,23 @@
 require 'sinatra'
 require 'simple_record'
 
+
+AWS_ACCESS_KEY_ID='AKIAIAFEKFHWPNPTZGZQ'
+AWS_SECRET_ACCESS_KEY='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'
+SimpleRecord.establish_connection(AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY)
+
+class Person < SimpleRecord::Base
+  has_attributes :name, :age
+end
+
 get '/' do
-  "Hi!"
+  start_time = Time.now
+  (1..1000).each do |i|
+    Person.create(:name => "#{i_name}", :age => i)
+  end
+  "#{Time.now - start_time}"
+end
+
+get '/count' do
+  "There are: #{Person.count} Persons"
 end
