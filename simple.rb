2be485acf4ddec7a63b1dfb773ba2fdc580f366a
Arfon Smith <arfon.smith@gmail.com>
2011-01-01 18:49:48 +0000

    titlecase

--- simple.rb
+++ simple.rb
@@ -1,5 +1,4 @@
 require 'rubygems'
-require 'titleize'
 require 'sinatra'
 require 'erb'
 
@@ -27,5 +26,12 @@ before do
     if post == requested
       @post = post
     end
+  end
+end
+
+class String
+  def titlecase
+    non_capitalized = %w{of etc and by the for on is at to but nor or a via}
+    gsub(/\b[a-z]+/){ |w| non_capitalized.include?(w) ? w : w.capitalize  }.sub(/^[a-z]/){|l| l.upcase }.sub(/\b[a-z][^\s]*?$/){|l| l.capitalize }
   end
 end
