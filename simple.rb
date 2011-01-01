69dde6d029f23414c053610a503fef450de79b14
Arfon Smith <arfon.smith@gmail.com>
2011-01-01 12:18:05 +0000

    Thesis page

--- simple.rb
+++ simple.rb
@@ -6,6 +6,10 @@ get '/?' do
   erb :index
 end
 
+get '/thesis?' do
+  erb :thesis
+end
+
 get '/autoscaling-on-aws-without-bundling-amis' do
   erb 'posts/autoscaling-on-aws-without-bundling-amis'.to_sym
 end
