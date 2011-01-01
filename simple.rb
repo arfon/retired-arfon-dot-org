a3d60c9f72c0f41ace1f34d3c17a3e3a4612b625
Arfon Smith <arfon.smith@gmail.com>
2011-01-01 19:25:21 +0000

    Links

--- simple.rb
+++ simple.rb
@@ -23,15 +23,8 @@ before do
   @posts = files.collect { |f| f.gsub('views/posts/', '').split('.').first }
   requested = request.path_info.gsub('/', '')
   @posts.each do |post|
-    if post == requested
+    if post.downcase == requested
       @post = post
     end
-  end
-end
-
-class String
-  def titlecase
-    non_capitalized = %w{of etc and by the for on is at to but nor or a via}
-    gsub(/\b[a-z]+/){ |w| non_capitalized.include?(w) ? w : w.capitalize  }.sub(/^[a-z]/){|l| l.upcase }.sub(/\b[a-z][^\s]*?$/){|l| l.capitalize }
   end
 end
