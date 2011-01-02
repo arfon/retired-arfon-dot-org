ebb0877abbddf80d0c8c7ddc2fd488d3eab9c02c
Arfon Smith <arfon.smith@gmail.com>
2011-01-02 09:40:43 +0000

    Datetimes

--- simple.rb
+++ simple.rb
@@ -1,6 +1,29 @@
 require 'rubygems'
 require 'sinatra'
 require 'erb'
+require 'hashie'
+require 'date'
+
+class Post < Hashie::Dash
+  property :raw_title
+  property :raw_posted_at
+  
+  def file_name
+    "views/posts/#{self.raw_title}.#{self.raw_posted_at}.html"
+  end
+  
+  def title
+    self.raw_title.gsub('-', ' ') 
+  end
+  
+  def posted_at
+    DateTime.parse(self.raw_posted_at)
+  end
+  
+  def formatted_posted_at
+    self.posted_at.strftime('%d %B %Y')
+  end
+end
 
 get '/?' do
   erb :index
@@ -12,7 +35,7 @@ end
 
 get '/*' do
   if @post
-    File.read "views/posts/#{@post}.html"
+    File.read @post.file_name
   else
     File.read 'views/404.html'
   end
@@ -20,10 +43,17 @@ end
 
 before do
   files = Dir.glob('views/posts/*.html')
-  @posts = files.collect { |f| f.gsub('views/posts/', '').split('.').first }
+  @posts = []
+  files.each do |file|
+    title_and_date = file.gsub('views/posts/', '').split('.')
+    @posts << Post.new(:raw_title => title_and_date[0], :raw_posted_at => title_and_date[1])
+  end
+    
+  @posts = @posts.sort { |a,b| b.posted_at <=> a.posted_at }  
+  
   requested = request.path_info.gsub('/', '')
   @posts.each do |post|
-    if post.downcase == requested
+    if post.raw_title.downcase == requested
       @post = post
     end
   end
